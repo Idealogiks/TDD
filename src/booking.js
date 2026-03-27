c3a9bffad71d81b54c4cf1a4240632f2698cf948
Gladys <bernard.gladys22@gmail.com>
2026-03-27 17:31:40 +0100

    feat: ajout code quand on veut ajouter reserv et reserv deja existante dans cette date

--- src/booking.js
+++ src/booking.js
@@ -20,6 +20,13 @@ function addBooking(booking, localStorage){
     if (new Date(booking.endDate) < new Date(booking.startDate)) {
         throw new ValidationError("End date cannot be before the start date");
     }
+
+    const existingBookings = localStorage.getBookings();
+    for (const existing of existingBookings) {
+        if ( new Date(booking.startDate) < new Date(existing.endDate) && new Date(booking.endDate) > new Date(existing.startDate)) {
+            throw new ValidationError("Booking conflicts with an existing booking");
+        }
+    }
 }
 
 
